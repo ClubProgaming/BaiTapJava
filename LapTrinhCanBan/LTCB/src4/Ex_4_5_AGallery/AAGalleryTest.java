55a2356f704f9a983a34e11400547e8675733918
tam <tam1442k3@gmail.com>
2021-12-22 14:00:47 +0700

    initial

--- LapTrinhCanBan/LTCB/src4/Ex_4_5_AGallery/AAGalleryTest.java
+++ LapTrinhCanBan/LTCB/src4/Ex_4_5_AGallery/AAGalleryTest.java
@@ -10,7 +10,10 @@ import junit.framework.TestCase;
  *
  */
 public class AAGalleryTest extends TestCase {
+	public void testConstructor() {
 	AAGallery c1 = new Text("Welcome.txt",5312,830);
 	AAGallery c2 = new MP3("theme.mp3",40960,20);
 	AAGallery c3 = new Images("flower.gif",57234,100,50,"medium");
+	}
+	
 }
